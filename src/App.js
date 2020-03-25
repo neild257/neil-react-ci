10dfb4b2c8ab6751748fba7e5e666fcba2cd6b8c
Harshit Pareek <hp1014@nyu.edu>
2020-03-25 12:01:58 +0530

    feat(adding the prod docker file)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!
+          hey there!
         </p>
         <a
           className="App-link"
